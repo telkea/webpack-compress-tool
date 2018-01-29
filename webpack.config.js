e22f0b3482b803327a62252bbae7d7e139eea337
Sébastien Lempens <slempens@telkea.com>
2018-01-29 15:59:13 +0100

    'webpack'

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'assets/js/')
     },
+    devtool:'source-map',
     module: {
         rules: [{
             test: /\.js$/,
